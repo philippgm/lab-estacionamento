110b215882dd6b4738fb0f110bcc3908b023f55b
Pedro Fernandes Freitas <fernandesfpedro@gmail.com>
2019-06-17 01:25:08 -0300

    Add 'Client' and 'Vehicle' models

--- estacionamento/placas/models.py
+++ estacionamento/placas/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Vehicle(models.Model):
+    owner = models.ForeignKey(Client, on_delete=models.CASCADE)
+    plate = models.CharField(max_length=7)
+    model = models.CharField(max_length=100)
+    manufacturer = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.manufacturer + ' ' + self.model + ' - ' + self.plate
